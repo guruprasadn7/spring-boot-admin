f400404b3f0500dcd629cce550c244acca2e5f45
Guruprasad <guruprasad@nichesolv.com>
2023-04-07 20:43:31 +0530

    Added authentication to spring boot admin

--- src/main/java/com/nichesolv/springboot/admin/config/SecuritySecureConfig.java
+++ src/main/java/com/nichesolv/springboot/admin/config/SecuritySecureConfig.java
@@ -1,22 +1,62 @@
 package com.nichesolv.springboot.admin.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
+@EnableWebSecurity
 public class SecuritySecureConfig {
 
-//    private final AdminServerProperties adminServer;
-//
-//    public SecuritySecureConfig(AdminServerProperties adminServer) {
-//        this.adminServer = adminServer;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors()
-//                .and()
-//                .authorizeHttpRequests()
-//                .anyRequest().permitAll();
-//        return http.build();
-//    }
+    @Value("${email}")
+    String email;
+    @Value("${password}")
+    String password;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        SavedRequestAwareAuthenticationSuccessHandler successHandler
+                = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl("/");
+        http.csrf().disable();
+        http.authorizeRequests()
+                .requestMatchers("/assets/**").permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/instances").permitAll()
+                .requestMatchers("/instances/**").permitAll()
+                .requestMatchers("/actuator").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/logout").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login")
+                .successHandler(successHandler).and()
+                .logout().logoutUrl("/logout").and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername(email)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
